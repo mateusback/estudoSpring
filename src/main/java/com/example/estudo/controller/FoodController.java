1f2acb66bbff04ce29c9737edd993412759d6c7d
Mateus Back <mateusfback14@gmail.com>
2023-07-19 09:42:48 -0300

    Estudos de Spring

---
+++ src/main/java/com/example/estudo/controller/FoodController.java
@@ -0,0 +1,32 @@
+package com.example.estudo.controller;
+
+import com.example.estudo.food.Food;
+import com.example.estudo.food.FoodRepository;
+import com.example.estudo.food.FoodRequestDTO;
+import com.example.estudo.food.FoodResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("food") //end-point
+public class FoodController {
+    @Autowired
+    private FoodRepository repository;
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data){
+        Food foodData = new Food(data);
+        repository.save(foodData);
+        return;
+    }
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping
+    public List<FoodResponseDTO> getAll(){
+
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
+    }
+}
---
+++ src/main/java/com/example/estudo/food/FoodRequestDTO.java
@@ -0,0 +1,4 @@
+package com.example.estudo.food;
+
+public record FoodRequestDTO(String title, String image, Integer price) {
+}
